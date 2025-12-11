8e63cd6f533c74e7b3bfca55b435896a29e0d8cc
FoxIPro5965 <Namthanh13.vn@gmail.com>
2025-12-11 19:59:18 +0700

    Update c00lBlock.lua

--- c00lBlock.lua
+++ c00lBlock.lua
@@ -404,7 +404,7 @@ local function detectAttack(char)
             local id = string.match(d.SoundId or "", "%d+")
             if id and attackIds[id] then
                 checked[char] = true
-                task.delay(0.5, function()
+                task.delay(0.08, function()
                     checked[char] = nil
                 end)
                 return true
